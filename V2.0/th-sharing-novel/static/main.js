a3fc66ee8e8102cff6992115989d553d01fdd443
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2019-01-15 17:50:02 +0700

    Create main.js

---
+++ V2.0/th-sharing-novel/static/main.js
@@ -0,0 +1,8 @@
+// Required ALL JS
+document.write('<script src="/templates/th-sharing-novel/static/js/jquery-3.3.1.min.js"></script>');
+document.write('<script src="/templates/th-sharing-novel/static/js/materialize.min.js"></script>');
+document.write('<script src="/templates/th-sharing-novel/static/js/aos.js"></script>');
+document.write('<script src="/templates/th-sharing-novel/static/js/yall-2.2.0.min.js"></script>');
+document.write('<script src="/templates/th-sharing-novel/static/js/scrolls.js"></script>');
+//document.write('');
+//document.write('');
